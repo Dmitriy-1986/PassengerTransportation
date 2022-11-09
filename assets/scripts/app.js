f8573bd048cd2b537ab5fe5673269965da867881
Dovgal Dima <52016913+Dmitriy-1986@users.noreply.github.com>
2022-11-10 01:04:46 +0200

    Add files via upload

---
+++ assets/scripts/app.js
@@ -0,0 +1,153 @@
+'use strict';
+
+let languageBar = document.querySelector('#languageBar'),
+    en = document.querySelector('#en'),
+    ua = document.querySelector('#ua'),
+    ru = document.querySelector('#ru'),
+    phone = document.querySelector('.phone'),
+    mail = document.querySelector('.mail'),
+    phoneFooter = document.querySelector('.phone-footer'),
+    mailFooter = document.querySelector('.mail-footer'),
+    logotype = document.querySelector('.logotype'),
+    titleHeader = document.querySelector('#titleHeader'),
+    navList = document.querySelector('#navList'),
+    mainAboutUs = document.querySelector('#mainAboutUs'),
+    sectionMainDescription = document.querySelector('#sectionMainDescription'),
+    buttonAboutUs = document.querySelector('#buttonAboutUs'),
+    imgMainAboutUs = document.querySelector('#imgMainAboutUs'),
+    mainServices = document.querySelector('#mainServices'),
+    sectionCards = document.querySelector('#sectionCards'),
+    cardImg = document.querySelector('.card-img'),
+    cardTitle = document.querySelector('.card-title'),
+    buttonCard = document.querySelector('.button-card'),
+    mainContacts = document.querySelector('#mainContacts'),
+    allLang = ['en', 'ua', 'ru'];
+
+phone.innerHTML = passengerTransportation.phone;
+phone.href = `tel:${passengerTransportation.phone}`;
+
+mail.innerHTML = passengerTransportation.mail;
+mail.href = `mailto:${passengerTransportation.mail}`;
+
+phoneFooter.innerHTML = passengerTransportation.phone;
+phoneFooter.href = `tel:${passengerTransportation.phone}`;
+
+mailFooter.innerHTML = passengerTransportation.mail;
+mailFooter.href = `mailto:${passengerTransportation.mail}`;
+
+en.addEventListener('click', urlLanguage);
+ua.addEventListener('click', urlLanguage);
+ru.addEventListener('click', urlLanguage);
+
+function urlLanguage() {
+    let lang = this.textContent;
+    location.href = window.location.pathname + '#' + lang;
+    location.reload();
+};
+
+function changeLanguage() {
+    let hash = window.location.hash;
+    hash = hash.substr(1);
+    console.log(hash);
+
+    if(!allLang.includes(hash)) {
+        location.href = window.location.pathname + '#ru';
+        location.reload(); 
+    };
+
+    document.querySelector('title').innerHTML = passengerTransportation['title'][hash];
+    
+    let mainMenuLoop =  passengerTransportation['mainMenu'][hash];
+    for(let i = 0; i < mainMenuLoop.length; i++) {
+        let list = document.createElement('li');
+            list.innerHTML += mainMenuLoop[i];
+            navList.append(list);
+    };
+
+    let articleHeaderLoop = passengerTransportation['articleHeader'][hash];
+    for(let c = 0; c < articleHeaderLoop.length; c++) {   
+        titleHeader.innerHTML =  `<span class="title">${articleHeaderLoop[0]}</span>`;
+        titleHeader.innerHTML += `<span class="subtitle">${articleHeaderLoop[1]}</span>`;   
+    };
+
+    let mainAboutUsLoop = passengerTransportation['aboutUs'][hash];
+    for(let x = 0; x < mainAboutUsLoop.length; x++) {   
+        mainAboutUs.innerHTML = mainAboutUsLoop[0];
+        sectionMainDescription.innerHTML =  mainAboutUsLoop[1];
+        buttonAboutUs.innerHTML =  mainAboutUsLoop[2]; 
+    };
+
+    let mainServicesLoop = passengerTransportation['services'][hash]['mainTitle'];
+    for(let a = 0; a < mainServicesLoop.length; a++) {   
+        mainServices.innerHTML = mainServicesLoop[0];
+    };
+
+    let sectionCardsLoop = passengerTransportation['services'][hash]['card'];
+    
+    sectionCardsLoop.forEach((el, i) => {
+        let card = document.createElement('div');
+            card.classList.add('card');
+            card.innerHTML += `
+                <div class="card-img">
+                    <img src="${sectionCardsLoop[i].imgSrc}" alt="NoImage">
+                </div>
+                
+                <h3 class="card-title">${sectionCardsLoop[i].title}</h3>
+                <p class="section-main-description">
+                    ${sectionCardsLoop[i].description}
+                </p>
+                <button class="button-card">${sectionCardsLoop[i].btn}</button>
+            `;
+
+        sectionCards.append(card);
+    });
+
+    let contactsTitleLoop = passengerTransportation['contactsTitle'][hash];
+    for(let y = 0; y < contactsTitleLoop.length; y++) {   
+        mainContacts.innerHTML = contactsTitleLoop[0];
+    };
+   
+}
+
+changeLanguage();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ assets/scripts/script.js
@@ -0,0 +1,210 @@
+'use strict';
+
+const passengerTransportation = {
+    "name": 'COMPANY',
+    "phone": '+8 (800) 123-45-67',
+    "mail": 'sales@log.com',
+    "language": ['en', 'ua', 'ru'],
+    "title": {
+        "en":  'Passenger Transportation',
+        "ua":  'Пассажирські Перевезення',
+        "ru":  'Пассажирские Перевозки',
+    },
+    "mainMenu": {
+        "en": [
+            'Services',
+            'About US',
+            'Contacts',
+        ],
+        "ua": [ 
+            'Сервіси',
+            'Про Нас',
+            'Контакти',
+        ],
+        "ru": [
+            'Сервисы',
+            'О Нас',
+            'Контакты',
+        ],
+    },
+    "articleHeader": {
+        "en": [
+            'TEXT CONTENT COMPANY',
+            'Subtitle text content dlkkldvbd dbdgvbgd dvbndjkl',
+        ],
+        "ua": [
+           'ТЕКСТ КОНТЕНТ КОМПАНІЇ',
+            'Сабтайтл текст контент на українській мові компанії',
+        ],
+        "ru": [
+           'ТЕКСТ КОНТЕНТ КОМПАНИИ',
+            'Сабтайтл текст контент на русском языке компании',
+        ],
+    },
+    "aboutUs": {
+        "en": [
+            'ABOUT US',
+            'Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium reiciendis inventore alias adipisci omnis ullam, commodi reprehenderit. Neque sequi deserunt perspiciatis dolor rerum est saepe cumque, quos et, eaque itaque?',
+            'more',
+        ],
+        "ua": [
+            'ПРО НАС',
+            'Але щоб ви зрозуміли, звідки виникає це хибне уявлення людей, цуратись насолоди і вихваляти страждання, я розкрию перед вами всю картину і роз’ясню, що саме говорив цей чоловік, який відкрив істину, якого я б назвав зодчим щасливого життя.',
+            'докладніше',
+        ],
+        "ru": [
+            'О НАС',
+            'Сайт рыбатекст поможет дизайнеру, верстальщику, вебмастеру сгенерировать несколько абзацев более менее осмысленного текста рыбы на русском языке, а начинающему оратору отточить навык публичных выступлений в домашних условиях. При создании генератора мы использовали небезизвестный универсальный код речей.',
+            'подробнее',
+        ],
+    },
+    "services": {
+        "en": {
+            "mainTitle": ['SERVICES'],
+            "card": [
+                {   
+                    "imgSrc": './assets/images/Rectangle-1.png',
+                    "title": 'Card',
+                    "description": 'Dinos were great!',
+                    "btn": 'more',
+                },
+                {
+                    "imgSrc": './assets/images/Rectangle-1.png',
+                    "title": 'Card',
+                    "description": 'Dinos were great!',
+                    "btn": 'more',
+                },
+                {
+                    "imgSrc": './assets/images/Rectangle-1.png',
+                    "title": 'Card',
+                    "description": 'Dinos were great!',
+                    "btn": 'more',
+                },
+                {
+                    "imgSrc": './assets/images/Rectangle-1.png',
+                    "title": 'Card',
+                    "description": 'Dinos were great!',
+                    "btn": 'more',
+                },
+                {
+                    "imgSrc": './assets/images/Rectangle-1.png',
+                    "description": 'Dinos were great!',
+                    "btn": 'more',
+                },
+                {
+                    "imgSrc": './assets/images/Rectangle-1.png',
+                    "title": 'Card',
+                    "description": 'Dinos were great!',
+                    "btn": 'more',
+                },
+            ]
+        },
+        "ua": {
+            "mainTitle": ['СЕРВІСИ'],
+            "card": [
+                {
+                    "imgSrc": './assets/images/Rectangle-1.png',
+                    "title": 'Картка',
+                    "description": 'Опис тексту відповідної картки!',
+                    "btn": 'докладніше',
+                },
+                {
+                    "imgSrc": './assets/images/Rectangle-1.png',
+                    "title": 'Картка',
+                    "description": 'Опис тексту відповідної картки!',
+                    "btn": 'докладніше',
+                },
+                {
+                    "imgSrc": './assets/images/Rectangle-1.png',
+                    "title": 'Картка',
+                    "description": 'Опис тексту відповідної картки!',
+                    "btn": 'докладніше',
+                },
+                {
+                    "imgSrc": './assets/images/Rectangle-1.png',
+                    "title": 'Картка',
+                    "description": 'Опис тексту відповідної картки!',
+                    "btn": 'докладніше',
+                },
+                {
+                    "imgSrc": './assets/images/Rectangle-1.png',
+                    "title": 'Картка',
+                    "description": 'Опис тексту відповідної картки!',
+                    "btn": 'докладніше',
+                },
+                {
+                    "imgSrc": './assets/images/Rectangle-1.png',
+                    "title": 'Картка',
+                    "description": 'Опис тексту відповідної картки!',
+                    "btn": 'докладніше',
+                },
+            ],
+        },
+        "ru": {
+            "mainTitle": ['СЕРВИСЫ'],
+            "card": [
+                {
+                    "imgSrc": './assets/images/Rectangle-1.png',
+                    "title": 'Карточка',
+                    "description": 'Описание какой - то карточки!',
+                    "btn": 'подробнее',
+                },
+                {
+                    "imgSrc": './assets/images/Rectangle-1.png',
+                    "title": 'Карточка',
+                    "description": 'Описание какой - то карточки!',
+                    "btn": 'подробнее',
+                },
+                {
+                    "imgSrc": './assets/images/Rectangle-1.png',
+                    "title": 'Карточка',
+                    "description": 'Описание какой - то карточки!',
+                    "btn": 'подробнее',
+                },
+                {
+                    "imgSrc": './assets/images/Rectangle-1.png',
+                    "title": 'Карточка',
+                    "description": 'Описание какой - то карточки!',
+                    "btn": 'подробнее',
+                },
+                {
+                    "imgSrc": './assets/images/Rectangle-1.png',
+                    "title": 'Карточка',
+                    "description": 'Описание какой - то карточки!',
+                    "btn": 'подробнее',
+                },
+                {
+                    "imgSrc": './assets/images/Rectangle-1.png',
+                    "title": 'Карточка',
+                    "description": 'Описание какой - то карточки!',
+                    "btn": 'подробнее',
+                },
+            ]
+        },
+    },
+    "contactsTitle": {
+        "en": ['CONTACTS'],
+        "ua": ['КОНТАКТИ'],
+        "ru": ['КОНТАКТЫ'],
+    },
+    "contacts": {
+        "facebook": {
+            "title": 'Facebook',
+            "link": './',
+        },
+        "viber": {
+            "title": 'Viber',
+            "link": './',
+        },
+        "telegramm": {
+            "title": 'Telegramm',
+            "link": './',
+        },
+        "whatsapp": {
+            "title": 'WhatsApp',
+            "link": './'
+        },
+    },
+};
+
+
