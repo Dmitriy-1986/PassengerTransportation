60aacf49a84ef5029b2807512d28c21723d6f3cd
Dovgal Dima <52016913+Dmitriy-1986@users.noreply.github.com>
2022-11-10 23:56:12 +0200

    Update app.js

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -46,7 +46,6 @@ function urlLanguage() {
 function changeLanguage() {
     let hash = window.location.hash;
     hash = hash.substr(1);
-    console.log(hash);
 
     if(!allLang.includes(hash)) {
         location.href = window.location.pathname + '#ru';
